b2a300029cf5b38f9b7a60a2bcf569c0b1f855e8
Nicolas Cleik <nicolascleick.tingui@gmail.com>
2025-11-14 15:55:32 -0300

    Conserto da funcionalidade de retrair a barra lateral

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('click', function() {
     const botaoToggle = document.querySelector('#superior-left button');
     
     const body = document.body;
