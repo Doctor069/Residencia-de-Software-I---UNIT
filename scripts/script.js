52228ee3261c6e63b9aba7ad4e9d5283647c58a3
Gustavo Araujo Costa <gugucosta2007@gmail.com>
2025-11-19 18:23:28 -0300

    Mudança do estilo do header

--- scripts/script.js
+++ scripts/script.js
@@ -13,20 +13,6 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }
 
-  const iconeTema = document.getElementById('icone-tema');
-  if (!iconeTema) {
-    console.warn("Ícone do tema não encontrado.");
-    return;
-  }
-
-  // 1. Aplicar tema salvo
-  const temaSalvo = localStorage.getItem('tema');
-  if (temaSalvo === 'light') {
-    body.classList.add('light-mode');
-    iconeTema.src = "/assets/icons/barra superior/temas/tema_escuro.svg";
-    iconeTema.alt = "Ativar modo escuro";
-  }
-
 function ativarTemaClaro() {
     const link = document.createElement('link');
     link.rel = "stylesheet";
@@ -41,27 +27,36 @@ function desativarTemaClaro() {
     if (link) link.remove();
 }
 
+  // 1. Aplicar tema salvo ao carregar
+  const temaSalvo = localStorage.getItem('tema');
+  if (temaSalvo === 'light') {
+    body.classList.add('light-mode');
+    ativarTemaClaro();
+  }
+
   // 2. Alternar tema ao clicar
-  iconeTema.addEventListener('click', (e) => {
+  document.addEventListener('click', function (e) {
+    const icone = e.target.closest('#lateral #icone-tema');
+    if (!icone) return;
     e.preventDefault();
     body.classList.toggle('light-mode');
 
-if (body.classList.contains('light-mode')) {
+    if (body.classList.contains('light-mode')) {
 
-    iconeTema.src = "/assets/icons/barra superior/temas/tema_escuro.svg";
-    iconeTema.alt = "Ativar modo escuro";
-    localStorage.setItem('tema', 'light');
+        icone.src = "/assets/icons/barra superior/temas/tema_escuro.svg";
+        icone.alt = "Ativar modo escuro";
+        localStorage.setItem('tema', 'light');
 
-    ativarTemaClaro(); // 🔥 carrega o arquivo
+        ativarTemaClaro(); // 🔥 carrega o arquivo
 
-} else {
+    } else {
 
-    iconeTema.src = "/assets/icons/barra superior/temas/tema_claro.svg";
-    iconeTema.alt = "Ativar modo claro";
-    localStorage.setItem('tema', 'dark');
+        icone.src = "/assets/icons/barra superior/temas/tema_claro.svg";
+        icone.alt = "Ativar modo claro";
+        localStorage.setItem('tema', 'dark');
 
-    desativarTemaClaro(); // ❄ remove o arquivo
-}
+        desativarTemaClaro(); // ❄ remove o arquivo
+    }
 
   });
 
